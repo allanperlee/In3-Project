98b58df8ffe4b5e93a7f954503ddbb0da88ee14f
allanperlee <48000848+allanperlee@users.noreply.github.com>
2020-09-25 16:25:08 -0700

    Using Python now for the backend to interact with the Ethereum network and the front-end. A simple GUI will be built for at least one feature which will start with checking the latest block on Ethereum

---
+++ test.py
@@ -0,0 +1,9 @@
+import in3
+
+in3_client = in3.Client()
+
+# Sends a request to the Incubed Network, that in turn will collect proofs from the Ethereum client, 
+# attest and sign the response, then send back to the client, that will verify signatures and proofs. 
+block_number = in3_client.eth.block_number()
+print(block_number) # Mainnet's block number
+
