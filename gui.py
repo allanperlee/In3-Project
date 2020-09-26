d7a9af05c17c3c466b0005496c7c1ed45ff55489
allanperlee <48000848+allanperlee@users.noreply.github.com>
2020-09-25 19:53:00 -0700

    Progress made for the GUI. Prints the last Ethereum block using In3.

--- gui.py
+++ gui.py
@@ -1,12 +1,17 @@
 import PySimpleGUI as sg
+from test import block_number
 
-layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("Ok")]]
+sg.theme('DarkAmber')   # Add a touch of color
+# All the stuff inside your window.
+layout = [[sg.Button("Print Ethereum's last block"), sg.Button('Cancel')] ]
 
-window = sg.Window("Demo", layout)
-
+# Create the Window
+window = sg.Window('Window Title', layout)
+# Event Loop to process "events" and get the "values" of the inputs
 while True:
-    event, values = window.read()
-    if event == "OK" or event == sg.WIN_CLOSED:
+    event = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
+    print('Last block: ', block_number)
 
 window.close()
