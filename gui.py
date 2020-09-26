52cf46531436d30ff5edf234a38434090674091e
allanperlee <48000848+allanperlee@users.noreply.github.com>
2020-09-26 12:31:40 -0700

    Updated the UI to print the latest block within the GUI as opposed to the Command Line. Will continue to modify the layout and/or add features to the GUI using the In3 API.

--- gui.py
+++ gui.py
@@ -3,15 +3,17 @@ from test import block_number
 
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
-layout = [[sg.Button("Print Ethereum's last block"), sg.Button('Cancel')] ]
+layout = [[sg.Button("Print Ethereum's last block"), sg.Button('Exit')],
+ ]
 
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Fetch Ethereum Data', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
-    event = window.read()
+    event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
-    print('Last block: ', block_number)
+    if event == "Print Ethereum's last block":
+        sg.Print(block_number)
 
 window.close()
