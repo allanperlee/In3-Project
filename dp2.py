47d26cd6d081192bcd0fc08a03749d7ffdaa8637
allanperlee <48000848+allanperlee@users.noreply.github.com>
2020-10-02 12:13:44 -0700

    Might be the final changes. One line of code was making the dp2.py file close after selecting submit

--- dp2.py
+++ dp2.py
@@ -8,10 +8,11 @@ sg.theme('Topanga')      # Add some color to the window
 # Very basic window.  Return values using auto numbered keys
 
 layout = [
-    [sg.Text('Please enter your Private Key, Amount, and Recipient Address')],
+    [sg.Text('Please enter your Private Key, Amount, Recipient, & Chain')],
     [sg.Text('Private Key', size=(15, 1)), sg.InputText()],
     [sg.Text('Amount', size=(15, 1)), sg.InputText()],
-    [sg.Text('Address', size=(15, 1)), sg.InputText()],
+    [sg.Text('Recipient', size=(15, 1)), sg.InputText()],
+    [sg.Text('Chain', size=(15, 1)), sg.InputText()],
     [sg.Submit(), sg.Cancel()]
 ]
 
@@ -22,7 +23,7 @@ receiver = ""
 value_in_wei = 0
 # None for Eth mainnet
 
-client = in3.Client('mainnet')
+
 # A transaction is only final if a certain number of blocks are mined on top of it.
 # This number varies with the chain's consensus algorithm. Time can be calculated over using:
 # wait_time = blocks_for_consensus * avg_block_time_in_secs
@@ -30,26 +31,23 @@ client = in3.Client('mainnet')
 confirmation_wait_time_in_seconds = 30
 etherscan_link_mask = 'https://{}{}etherscan.io/tx/{}'
 
-window = sg.Window('Ethereum Transaction with Incubed', layout)
-event, values = window.read()
-window.close()
-sender_secret = str(values[0])
-receiver = str(values[1])
-value_in_wei = values[2]  # the input data looks like a simple list when auto numbered
+window = sg.Window('Send Ether with In3', layout)
+
 
 # Event Loop to process "events" and get the "values" of the inputs
-while True:
+while True:                             # The Event Loop
     event, values = window.read()
-    
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        break
-    
-    else:
-        sg.Print('[.] Sending {} Wei from {} to {}. Please wait.\n'.format(tx.value, sender.address, tx.to))
+    sender_secret = str(values[0])
+    value_in_wei = values[1]  # the input data looks like a simple list when auto numbered
+    receiver = str(values[2])
+    chain = str(values[3])
+    client = in3.Client(chain)
+
+    if event == "Submit":
         try:
             sender = client.eth.account.recover(sender_secret)
             tx = in3.eth.NewTransaction(to=receiver, value=value_in_wei)
-            sg.Popup('[.] Sending {} Wei from {} to {}. Please wait.\n'.format(tx.value, sender.address, tx.to))
+            sg.Print('[.] Sending {} Wei from {} to {}. Please wait.\n'.format(tx.value, sender.address, tx.to))
             tx_hash = client.eth.account.send_transaction(sender, tx)
             sg.Print('[.] Transaction accepted with hash {}.'.format(tx_hash))
             add_dot_if_chain = '.' if chain else ''
@@ -71,5 +69,6 @@ while True:
         except in3.ClientException as e:
             sg.Print('Client returned error: ', str(e))
             sg.Print('Please try again.')
-
+    else:
+        break
 window.close()
